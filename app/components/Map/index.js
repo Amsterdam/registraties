4e4d23b7a78f71625041b6656a5a5c0941658a52
JJM van Deursen <janjaap@users.noreply.github.com>
2019-04-24 17:43:54 +0200

    Styles Map component and clears listeners when component is unmounted

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -1,53 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import amaps from 'amsterdam-amaps/dist/amaps';
+import 'amsterdam-amaps/dist/nlmaps/dist/assets/css/nlmaps.css';
 import 'leaflet/dist/leaflet.css';
-import 'static/nlmaps.css';
 
-import amaps from '../../static/amaps.iife';
-
-import './style.scss';
-
-const PREVIEW_ZOOM_LEVEL = 14;
-
-class Map extends React.Component {
+const MapContainer = styled.div`
+  height: 450px;
+  width: 100%;
+`;
+class Map extends Component {
   componentDidMount() {
+    const { center, onSearchSelect } = this.props;
+
     amaps.createMap({
-      center: {
-        latitude: this.props.latlng.latitude,
-        longitude: this.props.latlng.longitude,
-      },
-      layer: 'standaard',
+      center,
       target: 'mapdiv',
-      marker: false,
-      search: false,
-      zoom: PREVIEW_ZOOM_LEVEL,
+      search: true,
+      zoom: 13,
     });
+
+    amaps.on('search-select', searchSelect => {
+      onSearchSelect(searchSelect);
+    });
+  }
+
+  componentWillUnmount() {
+    amaps.on('search-select', () => {});
   }
 
   render() {
     return (
-      <div className="map-component">
-        <div className="row">
-          <div className="col-12">
-            <div className="map">
-              <div id="mapdiv" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <MapContainer>
+        <div id="mapdiv" style={{ height: '100%' }} />
+      </MapContainer>
     );
   }
 }
 
-Map.defaultProps = {
-  latlng: {},
-};
-
 Map.propTypes = {
-  latlng: PropTypes.shape({
-    latitude: PropTypes.string,
-    longitude: PropTypes.string,
-  }),
+  center: PropTypes.shape({
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+  }).isRequired,
+  onSearchSelect: PropTypes.func.isRequired,
 };
 
 export default Map;
