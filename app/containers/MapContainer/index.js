301784499c54835ac0e7b6d29bc8745d282284cb
JJM van Deursen <janjaap@users.noreply.github.com>
2019-04-24 17:45:03 +0200

    Has MapContainer redirect to results page when a value in the autosuggest field has been selected

--- app/containers/MapContainer/index.js
+++ app/containers/MapContainer/index.js
@@ -1,10 +1,22 @@
 import React, { Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
 import Map from '../../components/Map';
 
-const MapContainer = () => (
+const MapContainer = withRouter(({ history }) => (
   <Fragment>
-    <Map latlng={{ latitude: '52.372829', longitude: '4.900773' }} />
+    <Map
+      onSearchSelect={({ resultObject }) => {
+        const {
+          adresseerbaarobject_id: adresseerbaarobjectId,
+          nummeraanduiding_id: nummeraanduidingId,
+          openbareruimte_id: openbareruimteId,
+        } = resultObject;
+
+        history.push(`/${adresseerbaarobjectId}-${nummeraanduidingId}-${openbareruimteId}/`);
+      }}
+      center={{ latitude: 52.372829, longitude: 4.900773 }}
+    />
   </Fragment>
-);
+));
 
 export default MapContainer;
