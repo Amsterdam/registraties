f6498264874d438ae08c7f9b962a8d12ea8413a5
JJM van Deursen <janjaap@users.noreply.github.com>
2019-05-15 12:41:23 +0200

    Exports regexes

--- app/utils/index.js
+++ app/utils/index.js
@@ -6,6 +6,16 @@ const dateFields = ['begin_geldigheid', 'document_mutatie', 'einde_geldigheid', 
 const currencyFields = ['koopsom'];
 
 /**
+ * Kadastraal object nummer regular expression
+ */
+export const reKadastraalObjectNr = /([A-Z]{3}\d{2})\s?([A-Z]{1,2})\s?(\d{4,5})\s?(A|G)\s?(\d{1,4})/;
+
+/**
+ * Kadastraal object nummer regular expression with groups
+ */
+export const reKadastraalObjectNrGroups = /(?<gemeente>[A-Z]{3}\d{2})\s?(?<sectie>[A-Z]{1,2})\s?(?<objectNummer>\d{4,5})\s?(?<indexLetter>A|G)\s?(?<indexNummer>\d{1,4})/;
+
+/**
  * Object detector
  *
  * @param {Any} value
@@ -202,9 +212,4 @@ export const isValidSubjectNP = ({ geboortedatum, naam }) => !!geboortedatum && 
 
 export const isValidSubjectNNP = ({ kvknummer, rsin }) => !!kvknummer && !!rsin;
 
-const reKadastraalObjectNr = /[A-Z]{3}\d{2}\s?[A-Z]\s?\d{4,5}\s?(A|G)\s?\d{1,4}/;
-
-export const isAppartment = ({ aanduiding }) => {
-  const [, indexLetter] = aanduiding.match(reKadastraalObjectNr);
-  return indexLetter.toLowerCase() === 'a';
-};
+export const isAppartment = ({ indexletter }) => indexletter.toLowerCase() === 'a';
