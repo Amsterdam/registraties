3d8a959486ea32a633f293c22271252b71f14e43
JJM van Deursen <janjaap@users.noreply.github.com>
2019-05-21 13:36:16 +0200

    (Fix) Borken search This PR makes sure that the search saga isn't overwritten by the `Progress` container component

--- app/containers/Progress/index.js
+++ app/containers/Progress/index.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import injectSaga from 'utils/injectSaga';
-import injectReducer from 'utils/injectReducer';
 import Progress from 'components/Progress';
-import saga from 'containers/App/saga';
-import reducer from 'containers/App/reducer';
 import { makeSelectProgress } from 'containers/App/selectors';
 
-// eslint-disable-next-line
-class ProgressContainer extends Component {
-  render() {
-    const { progress } = this.props;
-
-    return <Progress now={progress} labelPosition="bottom" />;
-  }
-}
+const ProgressContainer = ({ progress }) => <Progress now={progress} labelPosition="bottom" />;
 
 ProgressContainer.propTypes = {
   progress: PropTypes.number.isRequired,
@@ -29,11 +17,5 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const withConnect = connect(mapStateToProps);
-const withReducer = injectReducer({ key: 'search', reducer });
-const withSaga = injectSaga({ key: 'search', saga });
 
-export default compose(
-  withReducer,
-  withSaga,
-  withConnect,
-)(ProgressContainer);
+export default withConnect(ProgressContainer);
