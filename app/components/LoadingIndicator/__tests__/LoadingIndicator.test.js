cd65b16f278d6b09978eb97c318c13b7b70c3a6d
JJM van Deursen <janjaap@users.noreply.github.com>
2019-05-22 10:08:30 +0200

    Adds LoadingIndictator component tests

---
+++ app/components/LoadingIndicator/__tests__/LoadingIndicator.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import LoadingIndicator from '..';
+
+describe('LoadingIndicator', () => {
+  afterEach(cleanup);
+
+  it('matches the snapshot', () => {
+    const { container } = render(<LoadingIndicator />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
