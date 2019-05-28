7d9392f7130fe066c6c0c4b0178bbb8e51931b3a
JJM van Deursen <janjaap@users.noreply.github.com>
2019-05-28 17:15:37 +0200

    (Fix) Webpack config This PR changes the webpack config so that specific settings only apply to the environment they should apply to.

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -40,6 +40,12 @@ module.exports = require('./webpack.base.babel')({
       exclude: /a\.js|node_modules/, // exclude node_modules
       failOnError: false, // show a warning when there is a circular dependency
     }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      HOST: 'localhost',
+      PORT: 8080,
+      HTTPS: false,
+    }),
   ],
 
   // Emit a source map for easier debugging
