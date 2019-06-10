3fc9942e2f5857bcf393288234bd2d2e9aa6877f
JJM van Deursen <janjaap@users.noreply.github.com>
2019-06-10 21:44:58 +0200

    BW-34 Homepage container tests Increases coverage

--- app/containers/HomePage/__tests__/HomePage.test.js
+++ app/containers/HomePage/__tests__/HomePage.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 
 import HomePage from '..';
-import messages from '../messages';
 
-describe('<HomePage />', () => {
-  it.skip('should render the page message', () => {
-    const renderedComponent = shallow(<HomePage />);
-    expect(renderedComponent.contains(<FormattedMessage {...messages.header} />)).toEqual(true);
+describe('containers/Homepage', () => {
+  it('should render a picture element', () => {
+    render(<HomePage />);
+    expect(document.getElementsByTagName('picture')).not.toBeNull();
   });
 });
