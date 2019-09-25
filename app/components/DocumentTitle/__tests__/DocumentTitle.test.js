66193688f624ffd138fef74db0d54969d46e0aca
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-09-25 16:11:07 +0200

    Remove unnecessary cleanup

--- app/components/DocumentTitle/__tests__/DocumentTitle.test.js
+++ app/components/DocumentTitle/__tests__/DocumentTitle.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render } from 'react-testing-library';
 
 import DocumentTitle from '..';
 
 describe('DocumentTitle', () => {
-  afterEach(cleanup);
-
   it('should set document title', () => {
     render(<DocumentTitle title="Capitalized with spaces" />);
     expect(document.title).toBe('Capitalized with spaces');
