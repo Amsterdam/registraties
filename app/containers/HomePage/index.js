9d4946ed78633e44e7cfe35bdafb218fc70a2e08
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-09-26 11:31:30 +0200

    Remove unnecessary React fragment

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,27 +11,25 @@ const Picture = styled.picture`
 `;
 
 const HomePage = () => (
-  <>
-    <Picture>
-      <source
-        media="(min-width: 700px)"
-        srcSet="//www.amsterdam.nl/publish/pages/830657/herengracht_met_fietsenrekken_eis_940x264.jpg"
-      />
-      <source
-        media="(max-width: 700px)"
-        srcSet="//www.amsterdam.nl/publish/pages/830657/700px/herengracht_met_fietsenrekken_eis_940x264.jpg"
-      />
-      <source
-        media="(max-width: 460px)"
-        srcSet="//www.amsterdam.nl/publish/pages/830657/460px/herengracht_met_fietsenrekken_eis_940x264.jpg"
-      />
-      <source
-        media="(max-width: 220px)"
-        srcSet="//www.amsterdam.nl/publish/pages/830657/220px/herengracht_met_fietsenrekken_eis_940x264.jpg"
-      />
-      <Img srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="" />
-    </Picture>
-  </>
+  <Picture>
+    <source
+      media="(min-width: 700px)"
+      srcSet="//www.amsterdam.nl/publish/pages/830657/herengracht_met_fietsenrekken_eis_940x264.jpg"
+    />
+    <source
+      media="(max-width: 700px)"
+      srcSet="//www.amsterdam.nl/publish/pages/830657/700px/herengracht_met_fietsenrekken_eis_940x264.jpg"
+    />
+    <source
+      media="(max-width: 460px)"
+      srcSet="//www.amsterdam.nl/publish/pages/830657/460px/herengracht_met_fietsenrekken_eis_940x264.jpg"
+    />
+    <source
+      media="(max-width: 220px)"
+      srcSet="//www.amsterdam.nl/publish/pages/830657/220px/herengracht_met_fietsenrekken_eis_940x264.jpg"
+    />
+    <Img srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="" />
+  </Picture>
 );
 
 export default HomePage;
